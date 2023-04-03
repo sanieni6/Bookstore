978c46aa39b971af624f014b5468676f6749c816
sanieni6 <sanieni6@gmail.com>
2023-04-03 15:35:37 -0500

    Add Redux Toolkit and Import the required libraries

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
 import './styles/index.css';
 
 const container = document.getElementById('root');
@@ -10,9 +13,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
