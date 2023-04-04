2feb5457a66c0fd8c78048115d7ac19422ed8349
sanieni6 <sanieni6@gmail.com>
2023-04-04 17:30:27 -0500

    Add dispatch action checkStatus

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
 
-const Categories = () => (
-  <div>
-    <button type="button">Status</button>
-  </div>
+const Categories = () => {
+  const { categoriesItems } = useSelector((store) => store.categories);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => dispatch(checkStatus())}
+      >
+        Status
+      </button>
+      <h3>{categoriesItems}</h3>
+    </div>
 
-);
+  );
+};
 
 export default Categories;
