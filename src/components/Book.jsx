492baedc09c7b86be6691846b9ea7f52a02bbc8c
sanieni6 <sanieni6@gmail.com>
2023-04-04 16:32:57 -0500

    Add new removeBook action

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Book.css';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-const Book = ({ title, author }) => (
-  <li className="book">
-    <h3 className="book-title">
-      {title}
-    </h3>
-    <h3 className="book-author">{author}</h3>
-  </li>
-);
+const Book = ({ title, author, id }) => {
+  const dispatch = useDispatch();
+  return (
+    <li className="book">
+      <h3 className="book-title">
+        {title}
+      </h3>
+      <h3 className="book-author">{author}</h3>
+      <button
+        type="button"
+        className="input-submit"
+        onClick={() => dispatch(removeBook(id))}
+      >
+        Delete
+      </button>
+    </li>
+  );
+};
+
 Book.propTypes = {
   title: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
   ]).isRequired,
   author: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]).isRequired,
+  id: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
   ]).isRequired,
